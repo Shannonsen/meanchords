67d9ff2eac42c986594747db8378fa314cb8b3f8
Shannonsen <a18016307@alumnos.uady.mx>
2022-11-06 14:30:46 -0600

    feature: add view register

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Layout from 'containers/Layout'
 import Login from 'containers/login/Login'
+import Register from 'containers/login/Register'
 import 'styles/global.css';
 
 const App = () => {
     return (
         <Layout>
             <Login />
+            <Register />
         </Layout>
     );
 }
--- src/containers/login/Login.jsx
+++ src/containers/login/Login.jsx
@@ -3,17 +3,18 @@ import 'styles/login.scss';
 
 const Login = () => {
 	return (
-		<div className="login"> 
+		<div className="login">
 			<div className="form-container">
 				<img src="./logos/logo_yard_sale.svg" alt="logo" className="logo" />
 				<h1 className="title">Ingresa tu cuenta</h1>
 				<p className="subtitle">Bienvenido a Mean Chords</p>
 				<form action="/" className="form">
+					<label className="label">Correo</label>
+					<input type="text" id="email" className="input" />
 					<label for="password" className="label">Password</label>
 					<input type="password" id="password" placeholder="*********" className="input input-password" />
-					<label for="new-password" className="label">Password</label>
-					<input type="password" id="new-password" placeholder="*********" className="input input-password" />
 					<input type="submit" value="Iniciar sesión" className="primary-button login-button" />
+					<input type="submit" value="Registrate" className="primary-button" />
 				</form>
 			</div>
 		</div>
